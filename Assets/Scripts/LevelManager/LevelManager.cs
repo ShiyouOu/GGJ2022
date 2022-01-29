ce240ed28a963973cc203c49bf6f12f8a0eb73c1
Shifuu <54868441+ShiyouOu@users.noreply.github.com>
2022-01-29 16:55:32 -0500

    Moving between room

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -4,35 +4,162 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager instance;
+
     // Am feelin lazy temp solution just for testing
-    [SerializeField] private List<RoomObject> _rooms;
+    [SerializeField] private List<FloorObject> _floors;
+    [SerializeField] private List<RoomObject> _roomsPool;
 
     [SerializeField] GameObject EnvironmentGroup;
 
-    private RoomObject _currentRoom;
+    private int _currentFloorLevel = 0;
+
+    private List<RoomObject> _roomObjects;
     private List<GameObject> _roomsClone;
-    private FloorRoom currentFloor;
+    private int _currentRoomIndex = 0;
+    private bool _movingRooms = false;
 
     public void MoveRoom(DoorDirection dir)
     {
-        foreach(GameObject roomObj in _roomsClone)
+        // So it only triggers once at a time
+        if (!_movingRooms)
         {
-            roomObj.SetActive(false);
+            _movingRooms = true;
+            FloorRoom newFloorRoom = GetFloorRoomByDirection(_floors[_currentFloorLevel].rooms[_currentRoomIndex].links, dir);
+            int newRoomIndex = GetRoomIndexByFloorRoom(newFloorRoom);
+
+            // Position the player correctly after newRoom
+            RoomInfo roomInfo = _roomsClone[newRoomIndex].GetComponent<RoomInfo>();
+            if (roomInfo)
+            {
+                DoorDirection doorDirection;
+
+                // Get which door we will leave from after entering new room
+                if (dir == DoorDirection.Up)
+                {
+                    doorDirection = DoorDirection.Down;
+                }
+                else if(dir == DoorDirection.Left)
+                {
+                    doorDirection = DoorDirection.Right;
+                }
+                else if(dir == DoorDirection.Right)
+                {
+                    doorDirection = DoorDirection.Left;
+                }
+                else
+                {
+                    doorDirection = DoorDirection.Up;
+                }
+                foreach (DoorInfo doorInfo in roomInfo.doors)
+                {
+                    if (doorInfo.direction == doorDirection)
+                    {
+                        GameObject.FindGameObjectWithTag("Player").transform.position = doorInfo.spawnLocation.transform.position;
+                    }
+                }
+            }
+            else
+            {
+                print("no room info");
+                GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(0, 0, 12);
+            }
+
+            // Display the new room
+            _roomsClone[_currentRoomIndex].SetActive(false);
+            _roomsClone[newRoomIndex].SetActive(true);
+            _currentRoomIndex = newRoomIndex;
+            _movingRooms = false;
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    // Find the next room based on current room and the direction we wish to move in
+    private FloorRoom GetFloorRoomByDirection(RoomLink[] roomLinks, DoorDirection dir)
     {
+        foreach(RoomLink link in roomLinks)
+        {
+            if (link.direction == dir)
+            {
+                return link.floorRoom;
+            }
+        }
+        return null;
+    }
+
+
+    // Get the room's index on the floor
+    private int GetRoomIndexByFloorRoom(FloorRoom fRoom)
+    {
+        int count = 0;
+        foreach(FloorRoom floorRoom in _floors[_currentFloorLevel].rooms)
+        {
+            if(floorRoom == fRoom)
+            {
+                return count;
+            }
+            count++;
+        }
+        return 0;
+    }
+
+    // Returns a random Room object based on a template
+    private RoomObject GetRandomRoomByType(RoomTemplate template)
+    {
+        List<RoomObject> roomsTypePool = new List<RoomObject>();
+        foreach (RoomObject roomObj in _roomsPool)
+        {
+            if(roomObj.template == template)
+            {
+                roomsTypePool.Add(roomObj);
+            }
+        }
+        print(roomsTypePool.Count);
+        RoomObject randomRoom = roomsTypePool[Random.Range(0, roomsTypePool.Count)];
+        return randomRoom;
+    }
+
+    private void ClearRooms()
+    {
+        foreach(GameObject roomObj in _roomsClone)
+        {
+            Destroy(roomObj);
+        }
+    }
+
+    public void LoadFloor(int floorNum)
+    {
+        _roomObjects = new List<RoomObject>();
         _roomsClone = new List<GameObject>();
-        foreach (RoomObject room in _rooms)
+        ClearRooms();
+
+        // Picks random rooms for each section of the floor
+        foreach (FloorRoom floorRoom in _floors[_currentFloorLevel].rooms)
+        {
+            RoomObject randomRoom = GetRandomRoomByType(floorRoom.template);
+            _roomObjects.Add(randomRoom);
+        }
+
+        // Create new instances of the rooms
+        foreach (RoomObject room in _roomObjects)
         {
             GameObject roomObj = Instantiate<GameObject>(room.roomPrefab);
             _roomsClone.Add(roomObj);
             roomObj.SetActive(false);
         }
+        // Set the main floor to active
         _roomsClone[0].SetActive(true);
-        _currentRoom = _rooms[0];
+        _currentRoomIndex = 0;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadFloor(_currentFloorLevel);
     }
 
     // Update is called once per frame
--- Assets/Scripts/LevelManager/RoomInfo.cs
+++ Assets/Scripts/LevelManager/RoomInfo.cs
@@ -14,4 +14,34 @@ public class DoorInfo
 public class RoomInfo : MonoBehaviour
 {
     public DoorInfo[] doors;
+
+    private void Update()
+    {
+        foreach(DoorInfo door in doors)
+        {
+            if (CheckHit(door.door))
+            {
+                LevelManager.instance.MoveRoom(door.direction);
+            }
+        }
+    }
+
+    private bool CheckHit(GameObject door)
+    {
+        Collider2D[] results = new Collider2D[5];
+        Collider2D collider = door.GetComponent<Collider2D>();
+        collider.OverlapCollider(new ContactFilter2D(), results);
+
+        foreach (Collider2D col in results)
+        {
+            if (!col) { break; }
+            Player playerTouched = col.gameObject.GetComponent<Player>();
+            if (playerTouched)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }

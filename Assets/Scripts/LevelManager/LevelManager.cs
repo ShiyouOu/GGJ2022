ea16c0ab03f88d8ee8e3cbc8786ca8b995f1b46f
Shifuu <54868441+ShiyouOu@users.noreply.github.com>
2022-01-29 22:32:04 -0500

    fixes

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -152,7 +152,10 @@ public class LevelManager : MonoBehaviour
         }
         // Set the main floor to active
         _roomsClone[0].SetActive(true);
-        Player.instance.RespawnPlayer();
+        if(_currentFloorLevel == 0)
+        {
+            Player.instance.RespawnPlayer();
+        }
         GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(0, 0, 12);
         _currentRoomIndex = 0;
         _levelLabel.SetText("Level: " + (_currentFloorLevel + 1));
@@ -170,6 +173,11 @@ public class LevelManager : MonoBehaviour
         {
             LoadFloor();
         }
+    }
+
+    public void SetFloorLevel(int num)
+    {
+        _currentFloorLevel = num;
     }
 
     private void Awake()
